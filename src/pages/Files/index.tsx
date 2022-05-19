0d3a626527de1bd5d7e044b20e9a60365cd64124
jeffersonw2020 <jeffersonsouza.w2020@gmail.com>
2022-05-19 20:35:47 -0300

    ciclo 02

--- src/pages/Files/index.tsx
+++ src/pages/Files/index.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
 import 'react-pro-sidebar/dist/css/styles.css';
-import { Container} from './styles';
+import { Container,TableContainer} from './styles';
 import Sidebar from '../../components/Sidebar';
+import pdfImg from '../../assets/ext-pdf.svg';
+import docImg from '../../assets/ext-doc.svg';
+//import { useAuth } from '../../hooks/auth';
 
 
 export default function Files() {
+  
+
   return (
     <Container>   
       <Sidebar/> 
+      <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Tamanho</th>
+                <th>Data de envio</th>
+                <th>Ação</th>
+              </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                 <div>
+                    <img className="iconTable" src={pdfImg} alt="" />
+                    <td className="title">                   
+                      arquivo.pdf</td>
+                  </div>
+                  <td>
+                    10 MB
+                  </td>
+                  <td>12/05/2022</td>
+                  <td>
+                  <button>
+                    ...
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                 <div>
+                    <img className="iconTable" src={docImg} alt="" />
+                    <td className="title">                   
+                      document.docx</td>
+                  </div>
+                  <td>
+                    2 MB
+                  </td>
+                  <td>12/05/2022</td>
+                  <td>
+                  <button>
+                    ...
+                    </button>
+                  </td>
+                </tr>
+            </tbody>
+          </table>
+        </TableContainer>
 
       
     </Container>
--- src/pages/Files/styles.ts
+++ src/pages/Files/styles.ts
@@ -8,6 +8,64 @@ export const Container = styled.div`
     background: #E5E5E5;
 `;
 
+export const TableContainer = styled.section`
+  margin-top: 30vh;
+  margin-left: 10vh;
+  table {
+    width: 100%;
+    border-spacing: 0 8px;
+    th {      
+      color: #000000;
+      font-weight: normal;
+      padding: 10px 32px;
+      text-align: left;
+      font-size: 16px;
+      line-height: 24px;
+
+      
+    }
+    div{
+        display: flex;
+        align-items: center;
+        cursor:pointer;
+        td{
+            margin-left: -25px;
+        }
+    }   
+    button{
+        padding: 0;
+        border: 0;
+        background: none;
+        margin-left: 20px;
+        margin-top: -10px;
+    } 
+    td {        
+      padding: 10px 30px;
+      border: 0;
+      font-size: 16px;
+      font-weight: normal;
+      color: #000000;      
+      
+      &.title {
+        color: #000000;
+      }
+      
+      &.income {
+        color: #12a454;
+      }
+      &.outcome {
+        color: #e83f5b;
+      }
+    }
+    td:first-child {
+      border-radius: 8px 0 0 8px;
+    }
+    td:last-child {
+      border-radius: 0 8px 8px 0;
+    }
+  }
+`;
+
 
 
 
